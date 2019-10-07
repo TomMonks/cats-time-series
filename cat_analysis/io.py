73748becd841d6d0619288e860afe732e836ab52
TomMonks <thomas.m.monks@gmail.com>
2019-10-07 10:21:41 +0100

    basic pre processing

---
+++ cat_analysis/io.py
@@ -0,0 +1,28 @@
+'''
+io
+
+Input output functions for CATS data
+
+Data are stored within individual trips files
+'''
+
+import os
+
+def read_trips(filepath):
+    """
+    Read in the names of ALL .csv files within the subdirectories contained within the 
+    filepath 
+    Returns a `List` of filenames and paths
+    
+
+    Keyword arguments:
+    filepath -- the filepath you want to search
+    
+    """
+    files = []
+    ext = '.csv'
+    for dirpath, dirnames, filenames in os.walk(filepath):
+        for filename in [f for f in filenames if f.endswith(ext)]:
+            files.append(os.path.join(dirpath, filename))
+            
+    return files
